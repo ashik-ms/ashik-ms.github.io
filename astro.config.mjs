9d2b348b6dedfb7078de9283b6df4b5f5f06e2b0
Ashik <ashik.salih@gmail.com>
2025-12-24 05:53:16 +0000

    Updated image paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+
 export default defineConfig({
-  site: 'https://ashik-ms.github.io'
+  site: 'https://ashik-ms.github.io',
+  base: '/astro-portfolio',
 });
