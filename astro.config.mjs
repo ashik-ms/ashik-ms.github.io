64dfef70749bbabbe628c723493acd023a420451
Ashik <ashik.salih@gmail.com>
2025-12-24 18:22:45 +0000

    Update
    Co-authored-by: Ona <no-reply@ona.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import { defineConfig } from 'astro/config';
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ashik-ms.github.io/astro-portfolio',
   base: '/astro-portfolio',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
